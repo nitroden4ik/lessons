258f77fdf29eb322bf88ff5da667b96d184aad11
Den Petrof <doogieman1992@gmail.com>
2023-05-17 20:34:47 +0300

    clear folder

--- lesson-11/script.js
+++
@@ -1,8 +0,0 @@
-const firstName = prompt('Введите ваше имя:');
-const lastName = prompt('Введите вашу фамилию:');
-const gender = prompt('Введите ваш пол:');
-const age = prompt('Введите ваш возраст:');
-
-const result = `Имя: ${firstName}\nФамилия: ${lastName}\nПол: ${gender}\nВозраст: ${age}`;
-
-alert(result);
